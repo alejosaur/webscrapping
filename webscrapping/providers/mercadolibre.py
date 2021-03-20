29d0f7bc254a4479c45b75cf33b3cf041bab3174
Alejandro <asantamariap@unal.edu.co>
2021-03-19 23:46:16 -0500

    chromedriver

--- webscrapping/providers/mercadolibre.py
+++ webscrapping/providers/mercadolibre.py
@@ -10,10 +10,10 @@ options.add_argument("--window-size=1920,1200")
 def get(url):
     try:
 	
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--disable-dev-shm-usage')
-        driver = webdriver.Chrome("./chromedriver",chrome_options=chrome_options)
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        driver = webdriver.Chrome("./chromedriver",chrome_options=options)
 
         url = url.split("?")[0]
         driver.get(url)
